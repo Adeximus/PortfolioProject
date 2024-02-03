0aeb9a6efcc67b31e3c3d8464da94ab16febe35c
Enoch <56989342+Adeximus@users.noreply.github.com>
2024-02-03 05:41:08 +0100

    Update SQLQuery1 portfolio.sql

--- SQLQuery1 portfolio.sql
+++ SQLQuery1 portfolio.sql
@@ -1,107 +1,142 @@
---Table Showing COVID-19 Infections and Deaths
-select *
-from PortfolioProject..CovidDeaths
+-- Table Showing COVID-19 Infections and Deaths
+SELECT *
+FROM PortfolioProject..CovidDeaths;
 
------------
---Examining the current ratio of total deaths to total cases
---Displaying the mortality rate following COVID-19 infection in, Nigeria.
-select location, date, population,total_cases, total_deaths,
-(convert(numeric, total_deaths)/ convert(numeric, total_cases) * 100) as DeathPercentage
-from PortfolioProject..CovidDeaths
-where continent is not null
-and location like '%Nigeria%'
-order by 1,2
+-- Examining the current ratio of total deaths to total cases
+-- Displaying the mortality rate following COVID-19 infection in Nigeria.
+SELECT 
+    location, 
+    date, 
+    population,
+    total_cases, 
+    total_deaths,
+    (CONVERT(NUMERIC, total_deaths) / CONVERT(NUMERIC, total_cases) * 100) AS DeathPercentage
+FROM PortfolioProject..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+    AND location LIKE '%Nigeria%'
+ORDER BY 1, 2;
 
-------------
---Examining the current ratio of total deaths to total cases
---Displaying the mortality rate following COVID-19 infection in, United States.
-select location, date, population,total_cases, total_deaths,
-(convert(numeric, total_deaths)/ convert(numeric, total_cases) * 100) as DeathPercentage
-from PortfolioProject..CovidDeaths
-where continent is not null
-and location like '%states%'
-order by 1,2
+-- Examining the current ratio of total deaths to total cases
+-- Displaying the mortality rate following COVID-19 infection in the United States.
+SELECT 
+    location, 
+    date, 
+    population,
+    total_cases, 
+    total_deaths,
+    (CONVERT(NUMERIC, total_deaths) / CONVERT(NUMERIC, total_cases) * 100) AS DeathPercentage
+FROM PortfolioProject..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+    AND location LIKE '%states%'
+ORDER BY 1, 2;
 
----------------
---Displaying the Infection ratio following COVID-19 Outbrake in Nigeria.
-select location, date, population,total_cases, 
-(convert(numeric, total_cases)/convert(numeric, population) * 100) as Infection_ratio
-from PortfolioProject..CovidDeaths
-where continent is not null
-and location like '%Nigeria%'
-order by 1,2
+-- Displaying the Infection ratio following COVID-19 Outbreak in Nigeria.
+SELECT 
+    location, 
+    date, 
+    population,
+    total_cases, 
+    (CONVERT(NUMERIC, total_cases) / CONVERT(NUMERIC, population) * 100) AS Infection_ratio
+FROM PortfolioProject..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+    AND location LIKE '%Nigeria%'
+ORDER BY 1, 2;
 
---Displaying the Infection ratio following COVID-19 Outbrake in United states.
-select location, date, population,total_cases, 
-(convert(numeric, total_cases)/convert(numeric, population) * 100) as Infection_ratio
-from PortfolioProject..CovidDeaths
-where continent is not null
-and location like '%states%'
-order by 1,2
+-- Displaying the Infection ratio following COVID-19 Outbreak in the United States.
+SELECT 
+    location, 
+    date, 
+    population,
+    total_cases, 
+    (CONVERT(NUMERIC, total_cases) / CONVERT(NUMERIC, population) * 100) AS Infection_ratio
+FROM PortfolioProject..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+    AND location LIKE '%states%'
+ORDER BY 1, 2;
 
--------------
---Displaying countries with the highest infection relative to their populations.
-select location,  population,MAX(cast(total_cases as int)), 
-(MAX(total_cases)/convert(numeric, population) * 100) as Infection_ratio
-from PortfolioProject..CovidDeaths
-where continent is not null
---and location like '%states%'
-group by population, location
-order by Infection_ratio desc
+-- Displaying countries with the highest infection relative to their populations.
+SELECT 
+    location,  
+    population,
+    MAX(CAST(total_cases AS INT)), 
+    (MAX(total_cases) / CONVERT(NUMERIC, population) * 100) AS Infection_ratio
+FROM PortfolioProject..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+GROUP BY population, location
+ORDER BY Infection_ratio DESC;
 
--------------
---Dispalying countries with the highest mortality relative to their population
-select location,  population,MAX(cast(total_cases as int)) as Mortality,
-(MAX(total_cases)/convert(numeric, population) * 100) as Mortality_ratio
-from PortfolioProject..CovidDeaths
-where continent is not null
---and location like '%states%'
-group by population, location
-order by Mortality_ratio desc
+-- Displaying countries with the highest mortality relative to their population.
+SELECT 
+    location,  
+    population,
+    MAX(CAST(total_cases AS INT)) AS Mortality,
+    (MAX(total_cases) / CONVERT(NUMERIC, population) * 100) AS Mortality_ratio
+FROM PortfolioProject..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+GROUP BY population, location
+ORDER BY Mortality_ratio DESC;
 
+-- Displaying countries with the highest infection.
+SELECT 
+    location, 
+    MAX(CAST(total_cases AS INT)) AS Infected
+FROM PortfolioProject..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+GROUP BY location
+ORDER BY Infected DESC;
 
---Displaying countries with the highest infection
-select location, MAX(cast(total_cases as int)) as Infected
-from PortfolioProject..CovidDeaths
-where continent is not null
---and location like '%states%'
-group by location
-order by Infected desc
+-- Displaying countries with the highest mortality.
+SELECT 
+    location, 
+    MAX(CAST(total_cases AS INT)) AS Mortality
+FROM PortfolioProject..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+GROUP BY location
+ORDER BY Mortality DESC;
 
+-- Breaking things down by continent.
+SELECT 
+    location,  
+    population,
+    MAX(CAST(total_cases AS INT)) AS Total_Mortality,
+    (MAX(total_cases) / CONVERT(NUMERIC, population) * 100) AS Mortality_ratio
+FROM PortfolioProject..CovidDeaths
+WHERE 
+    continent IS NULL
+GROUP BY population, location
+ORDER BY Mortality_ratio DESC;
 
---Dispalying countries with the highest mortality 
-select location, MAX(cast(total_cases as int)) as Mortality
-from PortfolioProject..CovidDeaths
-where continent is not null
---and location like '%states%'
-group by location
-order by Mortality desc
+-- Global numbers.
+SELECT 
+    date, 
+    SUM(CAST(new_cases AS NUMERIC)) AS Total_cases, 
+    SUM(CAST(new_deaths AS NUMERIC)) AS Total_deaths,
+    SUM(CAST(new_deaths AS NUMERIC)) / SUM(CAST(new_cases AS NUMERIC)) * 100 AS death_percentage
+FROM PortfolioProject..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+GROUP BY date
+ORDER BY 1;
 
------------
---BREAKING THINGS DOWN TO CONTINENT
-
-select location,  population,MAX(cast(total_cases as int)) as Total_Mortality,
-(MAX(total_cases)/convert(numeric, population) * 100) as Mortality_ratio
-from PortfolioProject..CovidDeaths
-where continent is null
---and location like '%states%'
-group by population, location
-order by Mortality_ratio desc
-
-------------
---GLOBAL NUMBERS
-select date, SUM(cast(new_cases as numeric))Total_cases, SUM(cast(new_deaths as numeric)) Total_deaths,
-SUM(cast(new_deaths as numeric))/SUM(cast(new_cases as numeric))*100 death_percentage
-from PortfolioProject..CovidDeaths
-where continent is not null
-group by date
-order by 1
-
---Displaying the total population vs Vaccination
-select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-SUM(cast(vac.new_vaccinations as numeric)) over (partition by dea.date)
-from PortfolioProject..CovidDeaths dea
-join PortfolioProject..CovidVacsinations vac
-on dea.location = vac.location
-and dea.date = vac.date 
-where dea.continent is not null
+-- Displaying the total population vs Vaccination.
+SELECT 
+    dea.continent, 
+    dea.location, 
+    dea.date, 
+    dea.population, 
+    vac.new_vaccinations,
+    SUM(CAST(vac.new_vaccinations AS NUMERIC)) OVER (PARTITION BY dea.date)
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVacsinations vac
+    ON dea.location = vac.location
+    AND dea.date = vac.date 
+WHERE 
+    dea.continent IS NOT NULL;
